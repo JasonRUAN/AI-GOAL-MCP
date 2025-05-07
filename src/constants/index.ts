fff6d2b83d0480800a4ae289198e6575f578e739
JasonRUAN <rzexin@gmail.com>
2025-05-07 23:20:34 +0800

    update url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -22,5 +22,7 @@ export const CONSTANTS = {
             aiGoalContract.aigTokenVaultSharedObjectId,
     },
     ELIZA_BASE_URL: "https://912a9ee0476329a340c214c0363d978bf252ff8c-3001.dstack-prod5.phala.network",
-    BACKEND_URL: "https://c46307fd94aaf26c0e093e1c12e615f2badc1192-5050.dstack-prod5.phala.network",
+    BACKEND_URL:
+        "https://7eaccb584f75db4d525e57b8ef7f1bb23581455b-5050.dstack-prod5.phala.network",
+
 };
