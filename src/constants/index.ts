ad3f8711112a85c91691a8dbb8e7309d5466dad8
JasonRUAN <rzexin@gmail.com>
2025-05-22 23:25:11 +0800

    使用SDK上传walrus存在问题,改用WALRUS.PUBLISHER_URL

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -21,6 +21,10 @@ export const CONSTANTS = {
         AIG_TOKEN_VAULT_SHARED_OBJECT_ID:
             aiGoalContract.aigTokenVaultSharedObjectId,
     },
+    WALRUS: {
+        PUBLISHER_URL: "https://publisher.walrus-testnet.walrus.space",
+        AGGREGATOR_URL: "https://aggregator.walrus-testnet.walrus.space",
+    },
     ELIZA_BASE_URL: "https://912a9ee0476329a340c214c0363d978bf252ff8c-3001.dstack-prod5.phala.network",
     BACKEND_URL:
         "https://7eaccb584f75db4d525e57b8ef7f1bb23581455b-5050.dstack-prod5.phala.network",
--- src/utils/walrus.ts
+++ src/utils/walrus.ts
@@ -1,6 +1,7 @@
 import type { Keypair } from "@mysten/sui/cryptography";
 import type { WalrusClient } from "@mysten/walrus";
 import * as fs from 'fs';
+import { CONSTANTS } from "../constants/index.js";
 
 export async function retrieveBlob(walrusClient: WalrusClient, blobId: string) {
 	const blobBytes = await walrusClient.readBlob({ blobId });
@@ -31,12 +32,46 @@ export async function writeFileBlob(
 	const fileBuffer = fs.readFileSync(file_path);
 	const fileBlob = new Uint8Array(fileBuffer);
 
-	const { blobId } = await walrusClient.writeBlob({
-		blob: fileBlob,
-		deletable: false,
-		epochs,
-		signer: keypair,
-	});
+	// const { blobId } = await walrusClient.writeBlob({
+	// 	blob: fileBlob,
+	// 	deletable: false,
+	// 	epochs,
+	// 	signer: keypair,
+	// });
+
+    const blobId = await storeBlob(fileBlob, epochs);
 
 	return blobId;
 }
+
+export const storeBlob = async (blobData: Uint8Array, epochs: number) => {
+    const url = `${CONSTANTS.WALRUS.PUBLISHER_URL}/v1/blobs?epochs=${epochs}`;
+    console.log(`storeBlob url: ${url}`);
+    return fetch(url, {
+        method: "PUT",
+        body: blobData,
+    }).then(async (response) => {
+        if (response.status === 200) {
+            const blobInfo = await response.json();
+
+            let blobId = "";
+            if (blobInfo.alreadyCertified) {
+                blobId = blobInfo.alreadyCertified.blobId;
+            } else if (blobInfo.newlyCreated) {
+                blobId = blobInfo.newlyCreated.blobObject.blobId;
+            } else {
+                throw new Error("Response does not contain expected bolbId");
+            }
+
+            return blobId;
+        }
+
+        throw new Error(
+            `Something went wrong when storing the blob!, ${JSON.stringify(
+                response,
+                null,
+                2
+            )}`
+        );
+    });
+};
